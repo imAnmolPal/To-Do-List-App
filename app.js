492599cb2470f0b3ccc1f52f62ff7d5926d9b3ff
Emma Paris <anmolpal1999@gmail.com>
2021-05-04 18:18:13 +0530

    set up project

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ const bodyParser = require("body-parser");
 const app = express(); // app constant by using express
 
 app.set("view engine", "ejs"); // tells our app to use EJS as its view engine. Always after declaring "app".
+
+
+app.get("/", function(req, res){
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay=== 0) { // getDay returns the day of the week, 0-Sunday to 6-Saturday
+        res.write("<h1>Yaaay it's the weekend!</h1>");
+    } else {
+        res.write("<p>It is not the weekend</p>");
+        res.write("<h1>Boo! I have to work!</h1>");
+        res.send();
+    }
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
