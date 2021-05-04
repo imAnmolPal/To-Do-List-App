3aacf257fcf238d36c2b66ffd70332c2efd181c3
Emma Paris <anmolpal1999@gmail.com>
2021-05-04 18:25:45 +0530

    set up day/date

--- app.js
+++ app.js
@@ -10,16 +10,49 @@ app.set("view engine", "ejs"); // tells our app to use EJS as its view engine. A
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    var currentDay = today.getDay();
 
-    if (currentDay === 6 || currentDay=== 0) { // getDay returns the day of the week, 0-Sunday to 6-Saturday
-        res.write("<h1>Yaaay it's the weekend!</h1>");
-    } else {
-        res.write("<p>It is not the weekend</p>");
-        res.write("<h1>Boo! I have to work!</h1>");
-        res.send();
+    var today = new Date();
+    // var currentDay = today.getDay();
+
+
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
     }
+
+    // if (currentDay === 6 || currentDay=== 0) { // getDay returns the day of the week, 0-Sunday to 6-Saturday
+    //     res.write("<h1>Yaaay it's the weekend!</h1>");
+    // } else {
+    //     res.write("<p>It is not the weekend</p>");
+    //     res.write("<h1>Boo! I have to work!</h1>");
+    //     res.send();
+    // }
+
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function () {
